5b3e6935769e9bf0755c36c92c21e1598909f351
havidtech <76119744+havidtech@users.noreply.github.com>
2022-12-27 22:54:37 +0100

    chore: update sdk index

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -5,10 +5,23 @@ const bridgeProviderCache = {};
 
 // Frontend will always call this function to create a bridgeProvider based on user selections
 // A cached bridgeProvider will be  returned  if already created  before for  efficiency
-export function createBridgeProvider(bridgeProviderId: string) {
+export function getBridgeProvider(bridgeProviderId: string) {
     if(bridgeProviderCache[bridgeId]) return bridgeProviderCache[bridgeProviderId];
+    return createBridgeProvider(bridgeProviderId);
+}
+
+function createBridgeProvider(bridgeProviderId: string) {
     const bridgeProvider = new BRIDGES[bridgeProviderId]();
     bridgeProviderCache[bridgeProviderId] = bridgeProvider;
 
     return bridgeProvider;
 }
+
+// Combine the  supported  assets of all bridge providers and return it. This function is
+// beyond the scope of any particular bridge provider
+export function allSupportedAssets() {
+
+}
+
+
+// ... Add other functions that frontend will need to call without using a specific bridge provider
