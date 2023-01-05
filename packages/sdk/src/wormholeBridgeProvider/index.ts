3b70a643e4490cb8f9c66a0738ef31783787653e
havidtech <76119744+havidtech@users.noreply.github.com>
2023-01-05 19:24:28 +0100

    chore: update structure of wormholeProvider

--- packages/sdk/src/wormholeBridgeProvider/index.ts
+++ packages/sdk/src/wormholeBridgeProvider/index.ts
@@ -1,45 +1,127 @@
-import { Assets, Networks } from "../../config";
-import { Quote, QuoteRequest, Update } from "../../types";
+import { CHAINS, CONTRACTS, ethers_contracts, hexToUint8Array, transferFromEthNative, tryNativeToHexString, tryNativeToUint8Array } from "@certusone/wormhole-sdk";
+import { nativeStringToHexAlgorand } from "@certusone/wormhole-sdk/lib/cjs/algorand";
+import { ethers, Signer } from "ethers";
+import { ALGORAND_WALLETS, Assets, BRIDGE_FROM_ETHEREUM_STATUS, Chains } from "../../config";
+import { NetworkType, Quote, QuoteRequest, Update } from "../../types";
 import { BaseBridgeProvider } from "../baseBridgeProvider";
+import { getNonAlgorandChain } from "../utils";
+import { EthereumUpdate } from "./types";
 
-const solAssets = Assets[Networks.SOL];
-const algoAssets = Assets[Networks.ALGO];
+
+const ethMainnetAssets = Assets.Mainnet.ETH;
+const ethTestnetAssets = Assets.Testnet.ETH;
+const algoMainnetAssets = Assets.Mainnet.ALGO;
+const algoTestnetAssets = Assets.Testnet.ALGO;
 
 export class WormHoleBridgeProvider implements BaseBridgeProvider  {
-   
+
+
     readonly supportedAssetsMaps = {
-        [Networks.SOL]: [[solAssets.xALGO?.symbol, algoAssets.ALGO?.symbol],  [] , [] ]
+        [NetworkType.MAINNET]: {
+            [Chains.ETH]: [[ethMainnetAssets.ETH.symbol, algoMainnetAssets.WETH.symbol]]
+        },
+        [NetworkType.TESTNET]: {
+            [Chains.ETH]: [[ethMainnetAssets.ETH.symbol, algoMainnetAssets.WETH.symbol]]
+        }
     }
 
-    public supportedNetworks() { 
-        return [Networks.ALGO, Networks.SOL];
+    public supportedChains(network: NetworkType) { 
+        return [Chains.ETH];
     }
 
-    public supportedAssetsByNetwork(network: string) {
-        return this.supportedAssetsMaps[network].map(assetMap => assetMap[0]);
+    public supportedAssetsByChain(chain: string, network: NetworkType) {
+        return this.supportedAssetsMaps[network][chain].map(assetMap => assetMap[0]);
     }
 
     public getQuote(quoteRequest: QuoteRequest) {
 
-        const nonAlgorandNetwork = quoteRequest.fromNetworkName === Networks.ALGO ? quoteRequest.toNetworkName : quoteRequest.fromNetworkName;
+        const nonAlgorandChain = getNonAlgorandChain({from: quoteRequest.fromChainName, to: quoteRequest.toChainName});
 
-        // Check if network is supported
-        if(!this.supportedNetworks().includes(nonAlgorandNetwork)) return null;
+        // Check if chain is supported
+        if(!this.supportedChains(quoteRequest.network).includes(nonAlgorandChain)) return null;
 
         // Check if asset is supported
-        if(!this.supportedAssetsByNetwork(nonAlgorandNetwork).includes(quoteRequest.assetName)) return null;
+        if(!this.supportedAssetsByChain(nonAlgorandChain, quoteRequest.network).includes(quoteRequest.assetName)) return null;
 
 
         // Get Quote ...
     }
 
     public moveAsset(quote: Quote)  {
-        
+        if(quote.fromChainName == Chains.ALGO) 
+            return this.moveAssetFromAlgorand(quote);
+
+        const nonAlgorandChain = getNonAlgorandChain({from: quote.fromChainName, to: quote.toChainName});
+        switch(nonAlgorandChain) {
+            case Chains.ETH: 
+                return this.moveAssetFromEthereum(quote);
+            default:
+                break;
+        }
     }
+
 
     public performNextStep(update: Update) {
-        
+        switch(update.quote.fromChainName){
+            case Chains.ETH:
+                return this.performNextEthereumStep(update as EthereumUpdate);
+            default:
+                return null;
+        }
     }
 
+    private moveAssetFromAlgorand(quote: Quote) {
+        switch(quote.fromWallet){
+            case ALGORAND_WALLETS.MyAlgo:
+                return this.moveUsingMyAlgoWallet(quote);
+            case ALGORAND_WALLETS.Pera:
+                return this.moveUsingPeraWallet(quote);
+            default:
+                return null;
+        }
+    }
+
+    private moveUsingMyAlgoWallet(quote: Quote) {
+
+    }
+
+    private moveUsingPeraWallet(quote: Quote) {
+
+    }
+
+    private async moveAssetFromEthereum(quote: Quote<ethers.providers.JsonRpcSigner>): Promise<EthereumUpdate> {
+
+        const TOKEN_BRIDGE = quote.network == NetworkType.MAINNET ? CONTRACTS.MAINNET.ethereum.token_bridge : CONTRACTS.TESTNET.ethereum.token_bridge;
+
+        const receipt = await transferFromEthNative(
+            TOKEN_BRIDGE,
+            quote.fromWallet as ethers.Signer,
+            quote.amountIn,
+            CHAINS.algorand,
+            hexToUint8Array(
+                tryNativeToHexString(
+                    quote.toAddress,
+                    CHAINS.algorand
+                ) || ""
+            ),
+        );
+
+        return {quote, receipt, status: BRIDGE_FROM_ETHEREUM_STATUS.WAITING_FOR_SEND_CONFIRMATION}
+    }
+
+    private performNextEthereumStep(update: EthereumUpdate){
+        switch(update.status){
+            case BRIDGE_FROM_ETHEREUM_STATUS.WAITING_FOR_SEND_CONFIRMATION:
+                this.waitForEthereumSendConfirmation(update.receipt);
+                return {...update, status: BRIDGE_FROM_ETHEREUM_STATUS.SEND_CONFIRMED};
+            
+            default:
+                return {...update, status: BRIDGE_FROM_ETHEREUM_STATUS.SUCCESS}
+        }
+    }
+
+    private waitForEthereumSendConfirmation(receipt: any){
+
+    }
 
 }
