08a9e7911379f430c17c9243b8c85c040a57fb76
Adebara Ayomide <adebaraayomide.com>
2023-01-13 20:45:26 +0100

    Chore: Reorganized the private functions

--- packages/sdk/src/glitterBridgeProvider/index.ts
+++ packages/sdk/src/glitterBridgeProvider/index.ts
@@ -47,149 +47,7 @@ export class GlitterBridgeProvider implements BaseBridgeProvider {
     public supportedAssetsByChain(chain: string, network: NetworkType) {
         return this.supportedAssetsMaps[network][chain].map(assetMap => assetMap[0]);
     }
-
-    private async getFee (asset: any) {
-        try {
-            const url = new URL(`${baseURL}/fee/${asset}`)
-            const response = await fetch(url);
-            if (!response.ok) {
-              throw new Error("Data not found");
-            }
-            const result = await response.json();
-            console.log(result)
-            return result;
-          } catch (err) {
-            console.error(err);
-          }
-    }
-
-    private  async getPrice (asset: any) {
-        try {
-            const url = new URL(`${baseURL}/release/${asset}`)
-            const response = await fetch(url);
-            if (!response.ok) {
-              throw new Error("Data not found");
-            }
-            const result = await response.json();
-            console.log(result)
-            return result;
-          } catch (err) {
-            console.error(err);
-          }
-    }
-
-    private async _getQuote (quoteRequest: QuoteRequest): Promise<Quote | null> {
-        
-        // xAlgo from solana to Algo
-        if (quoteRequest.assetName == solMainnetAssets.xALGO?.symbol) {
-
-            const xALGOFee = await this.getFee(solMainnetAssets.xALGO?.symbol);
-            const xALGORelease = await this.getPrice(solMainnetAssets.xALGO?.symbol) 
-            return {
-              ...quoteRequest,
-                assetName: algoMainnetAssets.ALGO?.symbol,
-                fromChainName: Chains.SOL,
-                toChainName: Chains.ALGO,
-                amountIn: quoteRequest.amountIn,
-                amountOut: xALGORelease * Number(quoteRequest.amountIn),
-                gasFeeEstimate: xALGOFee,
-                bridgeId: BridgeId.Glitter
-            }
-        } 
-
-        // Algo from Algorand to Solana 
-        else if (quoteRequest.assetName == algoMainnetAssets.ALGO?.symbol) {
-
-            const ALGOFee = await this.getFee(algoMainnetAssets.ALGO?.symbol);
-            const ALGORelease = await this.getPrice(algoMainnetAssets.ALGO?.symbol) 
-
-            return {
-              ...quoteRequest,
-                assetName: solMainnetAssets.xALGO?.symbol,
-                fromChainName: Chains.ALGO,
-                toChainName: Chains.SOL,
-                amountIn: quoteRequest.amountIn,
-                amountOut: ALGORelease * Number(quoteRequest.amountIn),
-                gasFeeEstimate: ALGOFee,
-                bridgeId: BridgeId.Glitter
-            }
-        }
-        
-        // SOl from solana to algorand
-        else if (quoteRequest.assetName == solMainnetAssets.SOLANA?.symbol) {
-            const SOLFee = await this.getFee(solMainnetAssets.SOLANA?.symbol);
-            const SOLRelease = await this.getPrice(solMainnetAssets.SOLANA?.symbol) 
-
-            return {
-              ...quoteRequest,
-                assetName: algoMainnetAssets.xSOL?.symbol,
-                fromChainName: Chains.SOL,
-                toChainName: Chains.ALGO,
-                amountIn: quoteRequest.amountIn,
-                amountOut: Number(quoteRequest.amountIn) * SOLRelease,
-                gasFeeEstimate: SOLFee,
-                bridgeId: BridgeId.Glitter
-            }
-        }
-
-
-        // xSOL from algorand to solana 
-        else if (quoteRequest.assetName == algoMainnetAssets.xSOL?.symbol) {
-            const xSOLFee = await this.getFee(algoMainnetAssets.xSOL?.symbol);
-            const xSOLRelease = await this.getPrice(algoMainnetAssets.xSOL?.symbol) 
-
-            return {
-              ...quoteRequest,
-                assetName: solMainnetAssets.SOLANA?.symbol,
-                fromChainName: Chains.ALGO,
-                toChainName: Chains.SOL,
-                amountIn: quoteRequest.amountIn,
-                amountOut: Number(quoteRequest.amountIn) * xSOLRelease,
-                gasFeeEstimate: xSOLFee,
-                bridgeId: BridgeId.Glitter
-            }
-
-        }
-
-        // USDCs from solana to Algorand
-        else if (quoteRequest.assetName  ==  solMainnetAssets.USDCs?.symbol) {
-            const USDcFee = await this.getFee( solMainnetAssets.USDCs?.symbol);
-            const USDcRelease = await this.getPrice(solMainnetAssets.USDCs?.symbol) 
-         
-                return {
-                        ...quoteRequest,
-                        assetName: algoMainnetAssets.USDCa?.symbol,
-                        fromChainName: Chains.SOL,
-                        toChainName: Chains.ALGO,
-                        amountIn: quoteRequest.amountIn,
-                        amountOut: Number(quoteRequest.amountIn) * USDcRelease,
-                        gasFeeEstimate: USDcFee,
-                        bridgeId: BridgeId.Glitter
-                      }
-        
-        }
-
-        // USDCa from algorand to Solana 
-        else if (quoteRequest.assetName == algoMainnetAssets.USDCa?.symbol) {
-            const USDaFee = await this.getFee( algoMainnetAssets.USDCa?.symbol);
-            const USDaRelease = await this.getPrice(algoMainnetAssets.USDCa?.symbol)
-
-            return {
-                ...quoteRequest,
-                assetName: solMainnetAssets.USDCs?.symbol,
-                fromChainName: Chains.ALGO,
-                toChainName: Chains.SOL,
-                amountIn: quoteRequest.amountIn,
-                amountOut: Number(quoteRequest.amountIn) * USDaRelease,
-                gasFeeEstimate: USDaFee,
-                bridgeId: BridgeId.Glitter
-            }
-        }
-
-        return null;
-        
-    }
-      
+   
 
     public async getQuote(quoteRequest: QuoteRequest): Promise<Quote | null> {
 
@@ -206,66 +64,6 @@ export class GlitterBridgeProvider implements BaseBridgeProvider {
         return await this._getQuote(quoteRequest)
 
     }
-
-    // Bridge Algo xAlgo from Algorand to Solana
-    private async bridgeAlgoToxAlgo (quote: Quote) {
-  
-      let startingBalance = await solana?.getTokenBalance(quote.toAddress, solMainnetAssets.xALGO?.symbol);
-      let bridged = await algorand?.bridge(quote.fromWallet, algoMainnetAssets.ALGO?.symbol, Chains.SOL,quote.toAddress, solMainnetAssets.xALGO?.symbol, Number(quote.amountIn));
-      let newBalance = await solana?.waitForTokenBalanceChange(quote.toAddress, "xAlgo", Number(startingBalance),90);
-      if (bridged == true) {
-        return {quote, status:BRIDGE_STATUS.SUCCESS}
-      }
-     else  {
-      return {quote, status:BRIDGE_STATUS.FAILED}
-     }
-     
-    }
-
-    // Bridge xAlgo to Algo from solana to Algorand
-    private async bridgexAlgoToAlgo (quote: Quote) {
-
-     let startingBalance = await algorand?.getBalance(quote.toAddress);
-     let bridged = await solana?.bridge(quote.fromWallet, solMainnetAssets.xALGO?.symbol, Chains.ALGO, quote.toAddress, algoMainnetAssets.ALGO.symbol, Number(quote.amountIn));
-     let newBalance =    await algorand?.waitForBalanceChange(quote.toAddress, Number(startingBalance),90);
-
-     if (bridged == true) {
-      return {quote, status:BRIDGE_STATUS.SUCCESS}
-    }
-   else  {
-    return {quote, status:BRIDGE_STATUS.FAILED}
-   }
-    }
-
-    // Bridge Sol to xsol from Solana to Algorand
-    private async bridgeSolToxSol (quote: Quote) {
-
-     let startingBalance = await algorand?.getTokenBalance(quote.toAddress, algoMainnetAssets.xSOL.symbol);
-     let bridged = await solana?.bridge(quote.fromWallet, solMainnetAssets.SOLANA?.symbol, Chains.ALGO, quote.toAddress, algoMainnetAssets.xSOL.symbol, Number(quote.amountIn));
-     let newBalance = await algorand?.waitForTokenBalanceChange(quote.toAddress, "xSOL", Number(startingBalance),90)
-
-     if (bridged == true) {
-      return {quote, status:BRIDGE_STATUS.SUCCESS}
-    }
-   else  {
-    return {quote, status:BRIDGE_STATUS.FAILED}
-   }
-    }
-    
-    //Bridge xSol to sol from Algorand to Solana
-    private async bridgexSolToSol (quote: Quote) {
-      let startingBalance = await solana?.getBalance(quote.toAddress);
-      let bridged = await algorand?.bridge(quote.fromWallet, algoMainnetAssets.xSOL.symbol, Chains.SOL, quote.toAddress, solMainnetAssets.SOLANA?.symbol, Number(quote.amountIn));
-      let newBalance = await solana?.waitForBalanceChange(quote.toAddress, Number(startingBalance),90);
-
-      if (bridged == true) {
-        return {quote, status:BRIDGE_STATUS.SUCCESS}
-      }
-     else  {
-      return {quote, status:BRIDGE_STATUS.FAILED}
-     }
-    }
-
 
     public async moveAsset(quote: Quote):Promise<Update> {
       if(quote.assetName == algoMainnetAssets.ALGO?.symbol){
@@ -289,4 +87,209 @@ export class GlitterBridgeProvider implements BaseBridgeProvider {
     public  performNextStep(update: any) {
 
     }
+
+    private async getFee (asset: any) {
+      try {
+          const url = new URL(`${baseURL}/fee/${asset}`)
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error("Data not found");
+          }
+          const result = await response.json();
+          console.log(result)
+          return result;
+        } catch (err) {
+          console.error(err);
+        }
+  }
+
+
+
+    private  async getPrice (asset: any) {
+      try {
+          const url = new URL(`${baseURL}/release/${asset}`)
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error("Data not found");
+          }
+          const result = await response.json();
+          console.log(result)
+          return result;
+        } catch (err) {
+          console.error(err);
+        }
+  }
+
+  private async _getQuote (quoteRequest: QuoteRequest): Promise<Quote | null> {
+      
+      // xAlgo from solana to Algo
+      if (quoteRequest.assetName == solMainnetAssets.xALGO?.symbol) {
+
+          const xALGOFee = await this.getFee(solMainnetAssets.xALGO?.symbol);
+          const xALGORelease = await this.getPrice(solMainnetAssets.xALGO?.symbol) 
+          return {
+            ...quoteRequest,
+              assetName: algoMainnetAssets.ALGO?.symbol,
+              fromChainName: Chains.SOL,
+              toChainName: Chains.ALGO,
+              amountIn: quoteRequest.amountIn,
+              amountOut: xALGORelease * Number(quoteRequest.amountIn),
+              gasFeeEstimate: xALGOFee,
+              bridgeId: BridgeId.Glitter
+          }
+      } 
+
+      // Algo from Algorand to Solana 
+      else if (quoteRequest.assetName == algoMainnetAssets.ALGO?.symbol) {
+
+          const ALGOFee = await this.getFee(algoMainnetAssets.ALGO?.symbol);
+          const ALGORelease = await this.getPrice(algoMainnetAssets.ALGO?.symbol) 
+
+          return {
+            ...quoteRequest,
+              assetName: solMainnetAssets.xALGO?.symbol,
+              fromChainName: Chains.ALGO,
+              toChainName: Chains.SOL,
+              amountIn: quoteRequest.amountIn,
+              amountOut: ALGORelease * Number(quoteRequest.amountIn),
+              gasFeeEstimate: ALGOFee,
+              bridgeId: BridgeId.Glitter
+          }
+      }
+      
+      // SOl from solana to algorand
+      else if (quoteRequest.assetName == solMainnetAssets.SOLANA?.symbol) {
+          const SOLFee = await this.getFee(solMainnetAssets.SOLANA?.symbol);
+          const SOLRelease = await this.getPrice(solMainnetAssets.SOLANA?.symbol) 
+
+          return {
+            ...quoteRequest,
+              assetName: algoMainnetAssets.xSOL?.symbol,
+              fromChainName: Chains.SOL,
+              toChainName: Chains.ALGO,
+              amountIn: quoteRequest.amountIn,
+              amountOut: Number(quoteRequest.amountIn) * SOLRelease,
+              gasFeeEstimate: SOLFee,
+              bridgeId: BridgeId.Glitter
+          }
+      }
+
+
+      // xSOL from algorand to solana 
+      else if (quoteRequest.assetName == algoMainnetAssets.xSOL?.symbol) {
+          const xSOLFee = await this.getFee(algoMainnetAssets.xSOL?.symbol);
+          const xSOLRelease = await this.getPrice(algoMainnetAssets.xSOL?.symbol) 
+
+          return {
+            ...quoteRequest,
+              assetName: solMainnetAssets.SOLANA?.symbol,
+              fromChainName: Chains.ALGO,
+              toChainName: Chains.SOL,
+              amountIn: quoteRequest.amountIn,
+              amountOut: Number(quoteRequest.amountIn) * xSOLRelease,
+              gasFeeEstimate: xSOLFee,
+              bridgeId: BridgeId.Glitter
+          }
+
+      }
+
+      // USDCs from solana to Algorand
+      else if (quoteRequest.assetName  ==  solMainnetAssets.USDCs?.symbol) {
+          const USDcFee = await this.getFee( solMainnetAssets.USDCs?.symbol);
+          const USDcRelease = await this.getPrice(solMainnetAssets.USDCs?.symbol) 
+       
+              return {
+                      ...quoteRequest,
+                      assetName: algoMainnetAssets.USDCa?.symbol,
+                      fromChainName: Chains.SOL,
+                      toChainName: Chains.ALGO,
+                      amountIn: quoteRequest.amountIn,
+                      amountOut: Number(quoteRequest.amountIn) * USDcRelease,
+                      gasFeeEstimate: USDcFee,
+                      bridgeId: BridgeId.Glitter
+                    }
+      
+      }
+
+      // USDCa from algorand to Solana 
+      else if (quoteRequest.assetName == algoMainnetAssets.USDCa?.symbol) {
+          const USDaFee = await this.getFee( algoMainnetAssets.USDCa?.symbol);
+          const USDaRelease = await this.getPrice(algoMainnetAssets.USDCa?.symbol)
+
+          return {
+              ...quoteRequest,
+              assetName: solMainnetAssets.USDCs?.symbol,
+              fromChainName: Chains.ALGO,
+              toChainName: Chains.SOL,
+              amountIn: quoteRequest.amountIn,
+              amountOut: Number(quoteRequest.amountIn) * USDaRelease,
+              gasFeeEstimate: USDaFee,
+              bridgeId: BridgeId.Glitter
+          }
+      }
+
+      return null;
+      
+  }
+
+  // Bridge Algo xAlgo from Algorand to Solana
+  private async bridgeAlgoToxAlgo (quote: Quote) {
+  
+    let startingBalance = await solana?.getTokenBalance(quote.toAddress, solMainnetAssets.xALGO?.symbol);
+    let bridged = await algorand?.bridge(quote.fromWallet, algoMainnetAssets.ALGO?.symbol, Chains.SOL,quote.toAddress, solMainnetAssets.xALGO?.symbol, Number(quote.amountIn));
+    let newBalance = await solana?.waitForTokenBalanceChange(quote.toAddress, "xAlgo", Number(startingBalance),90);
+    if (bridged == true) {
+      return {quote, status:BRIDGE_STATUS.SUCCESS}
+    }
+   else  {
+    return {quote, status:BRIDGE_STATUS.FAILED}
+   }
+   
+  }
+
+  // Bridge xAlgo to Algo from solana to Algorand
+  private async bridgexAlgoToAlgo (quote: Quote) {
+
+   let startingBalance = await algorand?.getBalance(quote.toAddress);
+   let bridged = await solana?.bridge(quote.fromWallet, solMainnetAssets.xALGO?.symbol, Chains.ALGO, quote.toAddress, algoMainnetAssets.ALGO.symbol, Number(quote.amountIn));
+   let newBalance =    await algorand?.waitForBalanceChange(quote.toAddress, Number(startingBalance),90);
+
+   if (bridged == true) {
+    return {quote, status:BRIDGE_STATUS.SUCCESS}
+  }
+ else  {
+  return {quote, status:BRIDGE_STATUS.FAILED}
+ }
+  }
+
+  // Bridge Sol to xsol from Solana to Algorand
+  private async bridgeSolToxSol (quote: Quote) {
+
+   let startingBalance = await algorand?.getTokenBalance(quote.toAddress, algoMainnetAssets.xSOL.symbol);
+   let bridged = await solana?.bridge(quote.fromWallet, solMainnetAssets.SOLANA?.symbol, Chains.ALGO, quote.toAddress, algoMainnetAssets.xSOL.symbol, Number(quote.amountIn));
+   let newBalance = await algorand?.waitForTokenBalanceChange(quote.toAddress, "xSOL", Number(startingBalance),90)
+
+   if (bridged == true) {
+    return {quote, status:BRIDGE_STATUS.SUCCESS}
+  }
+ else  {
+  return {quote, status:BRIDGE_STATUS.FAILED}
+ }
+  }
+  
+  //Bridge xSol to sol from Algorand to Solana
+  private async bridgexSolToSol (quote: Quote) {
+    let startingBalance = await solana?.getBalance(quote.toAddress);
+    let bridged = await algorand?.bridge(quote.fromWallet, algoMainnetAssets.xSOL.symbol, Chains.SOL, quote.toAddress, solMainnetAssets.SOLANA?.symbol, Number(quote.amountIn));
+    let newBalance = await solana?.waitForBalanceChange(quote.toAddress, Number(startingBalance),90);
+
+    if (bridged == true) {
+      return {quote, status:BRIDGE_STATUS.SUCCESS}
+    }
+   else  {
+    return {quote, status:BRIDGE_STATUS.FAILED}
+   }
+  }
+
+    
 }
