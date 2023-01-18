04fac82060e745a40da7722cc513bb8925de669e
dicetheDev <samuelblessing125@gmail.com>
2023-01-18 03:13:42 +0100

    changes

--- apps/frontend/src/pages/Home.js
+++ apps/frontend/src/pages/Home.js
@@ -29,7 +29,7 @@ const Home = () => {
         <Box w={{base:"100%", md:"100%", lg:"85%"}} mt="6rem" mx="auto">
             <Text w={{base:"100%", md:"100%", lg:"554px"}} mb={2} fontSize={{base:"2.2rem", md:"34px", lg:"38.8px"}} fontWeight="700" className='leading-[46px] font-[syne]'> Effortlessly connect different blockchains with Chainhooper </Text>
             <Text className='leading-[24px] font-[syne]' mb={5}>Are you tired of juggling multiple bridge solutions to transfer your assets between different blockchains? Our bridge aggregator simplifies the process by curating the best route for you. Simply specify your source and destination chains, and we'll handle the rest</Text>
-            {isConnected ? (
+            {isConnect ? (
                 <Box onClick={connectWallet} display={{base:"none", md:"block"}}>
                     <Btn text="connect wallet" />
                 </Box>
