c280c10c8ed6249a0a3495237540bcce595cd177
havidtech <76119744+havidtech@users.noreply.github.com>
2023-02-02 13:59:37 +0100

    fix: add time estimates for wormhole

---
+++ packages/sdk/src/wormholeBridgeProvider/timeEstimates.ts
@@ -0,0 +1,7 @@
+import { Chains } from "../config";
+
+// Waiting Times in seconds
+export const waitingTimes = {
+    [Chains.ETH]: {low: 900, high: 1800},
+    [Chains.ETH]: {low: 3.7, high: 5}
+}
