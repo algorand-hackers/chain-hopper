61b490a8fb5ddf2a756299e8e37ddc84e367b6c7
dicethedev <dicethedev@gmail.com>
2023-02-07 17:31:36 +0100

    implemented some update on connect wallet

--- apps/frontend/src/components/ConnectWallet/ConnectWallet.js
+++ apps/frontend/src/components/ConnectWallet/ConnectWallet.js
@@ -10,13 +10,10 @@ import {
   Image,
   useColorMode
 } from '@chakra-ui/react';
-import metamask from '../../asset/metamask.svg';
-import coinbase from '../../asset/coinbase.svg';
-import walletConnect from '../../asset/walletConnect.svg';
+import metamask from '../../asset/metamaskLogo.svg';
+import phantomLogo from '../../asset/phantomLogo.svg';
 // import { toast } from 'react-toastify';
 import Images_Icons from '../../constant/icons-images';
-// import {PeraWalletConnect} from "@perawallet/connect"
-// import MyAlgoConnect from '@randlabs/myalgo-connect';
 
 // const wallet = [
 //   {
@@ -43,83 +40,12 @@ const ConnectWallet = ({
   connectWallet,
   walletIcon,
   connectToMyAlgo,
+  connectPhantom,
+  phantom,
   isOpen,
   onOpen,
   onClose,
 }) => {
-
-  // const [isActive, setIsActive] = useState(false);
-  // const [icon, setIcon] = useState(false);
-  
-  // Store account address which is connected dApp with Pera Wallet
-  // const [accountAddress, setAccountAddress] = (useState < string) | (null > null);
-  //  // Check app is connected with Pera Wallet
-	// const isConnectedToPeraWallet = !!accountAddress;
-  
-   //useEffect for Pera Wallet
-  // useEffect(() => {
-  //   // Reconnect to the session when the component is mounted
-  //   peraWallet.reconnectSession().then((accounts) => {
-  //     // Setup the disconnect event listener
-  //     peraWallet.connector?.on("disconnect", handleDisconnectWalletClick);
-
-  //     if (accounts.length) {
-  //       setAccountAddress(accounts[0]);
-  //     }
-  //   });
-  // }, []);
-  // const {  connectWallet } = useContext(TransactionContext);
-
-  // function handleConnectWalletClick() {
-  //   peraWallet
-  //     .connect()
-  //     .then((newAccounts) => {
-  //       // Setup the disconnect event listener
-  //       peraWallet.connector?.on("disconnect", handleDisconnectWalletClick);
-
-  //       setAccountAddress(newAccounts[0]);
-  //     })
-  //     .reject((error) => {
-  //       // You MUST handle the reject because once the user closes the modal, peraWallet.connect() promise will be rejected.
-  //       // For the async/await syntax you MUST use try/catch
-  //       if (error?.data?.type !== "CONNECT_MODAL_CLOSED") {
-  //         // log the necessary errors
-  //         console.log(error);
-  //          toast.error('error connecting to Pera wallet', {
-  //          position: toast.POSITION.TOP_CENTER, 
-  //          autoClose: 5000
-  //         });
-  //       }
-  //     });
-  // }
-
-//   function handleDisconnectWalletClick() {
-// 	peraWallet.disconnect();
-// 	setAccountAddress(null);
-// }
-
-  // const myAlgoConnect = new MyAlgoConnect({ disableLedgerNano: false });
-
-  //  const settings = {
-  //      shouldSelectOneAccount: false,
-  //      openManager: false
-  //  };
-
-  //  async function connectToMyAlgo() {
-  //    try {
-  //      const accounts = await myAlgoConnect.connect(settings);
-  //      console.log(accounts)
-  //      const addresses = accounts.map(account => account.address);
-  //      console.log(addresses)
-    
-  //    } catch (err) {
-  //      console.error(err);
-  //      toast.error('something went wrong', {
-  //       position: toast.POSITION.TOP_RIGHT,
-  //       autoClose: 2000
-  //     });
-  //    }
-  //  }
 
   const { colorMode } = useColorMode();
 
@@ -205,6 +131,32 @@ const ConnectWallet = ({
                 <Box>
                   <Text mt="4px" textAlign={'left'} fontSize="16px">
                    MyAlgo
+                  </Text>
+                </Box>
+              </Flex>
+
+                 {/* -------------------- My Phantom Wallet ------------------------ */}
+                 <Flex
+                direction={'column'}
+                display="flex"
+                justify="center"
+                alignItems="center"
+                onClose={onClose}
+                onClick={connectPhantom}
+                _hover={ colorMode === 'light' ? { bg: '#EFF6FF', borderRadius: '16px', color: "black" } :
+                { bg: '#EFF6FF', borderRadius: '16px', color: "black" }
+                }
+                align="center"
+                as="button"
+                // key={item.nanoid}
+                w="30%"
+                mb={8}
+                p={4}
+              >
+                <Image src={phantomLogo} alt="phantom-logo" />
+                <Box>
+                  <Text mt="4px" textAlign={'left'} fontSize="16px">
+                   Phantom
                   </Text>
                 </Box>
               </Flex>
