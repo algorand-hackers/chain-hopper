422e78bb11edbcc7fc6f4bfe7469c40db704048b
EnebeliEmmanuel <iemmaenebeli@gmail.com>
2023-02-13 10:12:08 +0100

    merge

--- apps/frontend/src/context/TransactionContext.jsx
+++ apps/frontend/src/context/TransactionContext.jsx
@@ -8,6 +8,8 @@ import phantomLogoM from '../asset/phantomLogo.svg';
 import Images_Icons from '../constant/icons-images';
 import { getSolBalance } from './main';
 import { Chains, NetworkType } from '@chain-hopper/sdk';
+import { Phantom } from '@aphantom-ds/phantom-js';
+
 
 
 export const TransactionContext = React.createContext();
@@ -15,6 +17,9 @@ export const TransactionContext = React.createContext();
 const { ethereum, solana } = window;
 
 export const TransactionsProvider = ({ children }) => {
+
+  const MainnetID = '0x1';
+  const TestnetID = '0x2';
 
   const [otherChainAccount, setOtherChainAccount] = useState('');
   const [algorandAccount, setAlgorandAccount] = useState('');
@@ -35,62 +40,7 @@ export const TransactionsProvider = ({ children }) => {
 
   
 
-  // const checkIfWalletIsConnect = async () => {
-  //   // if (typeof window != "undefined" && typeof window.ethereum != "undefined"){
-  //   //   try {
-  //   //     // if (!ethereum) return 
-  //   //     // alert("Please install MetaMask.")
-       
-  //   //     // const provider = new ethers.providers.Web3Provider(ethereum);
-  //   //     const accounts = await ethereum.request({ method: 'eth_accounts' });
   
-  //   //     if (accounts.length) {
-  //   //       //  setCurrentAccount(await provider.lookupAddress(accounts[0]));
-  //   //       setCurrentAccount(accounts[0]);
-  //   //       //         var address = '0x1234...';
-  //   //       // var name = await provider.lookupAddress(address);
-  //   //       // // ethers.js automatically checks that the forward resolution matches.
-  //   //     } else {
-  //   //       console.log('No accounts found');
-  //   //     }
-  //   // }catch (error) {
-  //   //   console.log(error);
-  //   // }else{
-  //   //        console.log("Please install Metamask");
-  //   //        toast.warning('Please install Metamask', {
-  //   //        position: toast.POSITION.TOP_CENTER, 
-  //   //        autoClose: 5000
-  //   //       });
-  //   //     }
-  //   if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
-  //         // console.log("clicked metamask")
-  //          try {
-  //               /* Metamask is installed */
-  //               const accounts = await ethereum.request({ method: 'eth_accounts' });
-  
-  //               if (accounts.length) {
-  //                       //  setCurrentAccount(await provider.lookupAddress(accounts[0]));
-  //                       setCurrentAccount(accounts[0]);
-  //                       //         var address = '0x1234...';
-  //                       // var name = await provider.lookupAddress(address);
-  //                       // // ethers.js automatically checks that the forward resolution matches.
-  //                     } else {
-  //                       console.log('No accounts found');
-  //                     }
-  //          } catch (err) {
-  //               console.error(err.messages);
-  //          }
-  //       } else {
-  //         /* Metamask is not installed */
-  //         if(localStorage.getItem("wallet")){
-  //           setCurrentAccount(localStorage.getItem("wallet"));
-  //         }
-  //          toast.info('Please install Metamask', {
-  //          position: toast.POSITION.TOP_CENTER, 
-  //          autoClose: 2000
-  //         });
-  //       }
-  // };
 
 
 
